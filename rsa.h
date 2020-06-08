fb068502985111db8095455c2278b12352c05061
Aurore Chappuis <aurorechappuis@macbook-pro-de-aurore.home>
2020-06-08 13:52:00 +0200

    modif des headers

--- rsa.h
+++ rsa.h
@@ -2,27 +2,27 @@
 #define rsa_h
 
 // the gcd function 
-int gcd(int a, int b);
+int gcd(int, int);
 
 // Compute the inverse of a (mod b) thanks to the extended euclidean algorithm
-int modularMultiplicativeInverse(int a, int b);
+int modularMultiplicativeInverse(int, int);
 
 // Generate the RSA encryption's public keys, the KeyModulus and the KeyExponent, thanks to help 
 // of two prime numbers
-void publicKeyGenerator(int* publicKeyModulus, int* publicKeyExponent, int primeNumber1, int primeNumber2);
+void publicKeyGenerator(int*, int*, int, int);
 
 // Compute a^k (mod n)
-int modularExponentiation(int a, int k, int n);
+int modularExponentiation(int, int, int);
 
 // Generate the RSA decryption's private key, the KeyExponent, thanks to help 
 // of two prime numbers and the publicKeyExponent
-void privateKeyGenerator(int* privateKeyExponent, int publicKeyExponent, int primeNumber1, int primeNumber2);
+void privateKeyGenerator(int*, int, int, int);
 
 // Encryption of a message thanks to the RSA system
-int* RSAencryption(char* message, int messageSize, int publicKeyExponent, int publicKeyModulus);
+int* RSAencryption(char*, int, int, int);
 
 // Decryption of a encrypted message with the RSA system
-char* RSAdecryption(int* message, int messageSize, int privateKeyExponent, int publicKeyModulus);
+char* RSAdecryption(int*, int, int, int);
 
 
 #endif /* rsa_h */
