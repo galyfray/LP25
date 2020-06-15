a3635d539d5fbc1f010d119f7a9b3e4788216af0
Nexowo <49556588+Nexowo@users.noreply.github.com>
2020-06-15 14:25:13 +0200

    Commentaires

--- GUI/rechercheMenu.c
+++ GUI/rechercheMenu.c
@@ -4,14 +4,14 @@ void rechercheMenu(S_menu* listeMenu,int nbrMenu, int nbMenu){
     char ligne[30];
     FILE* fichier = NULL;
 
-    fichier = fopen("liste_menus.txt", "r");
+    fichier = fopen("liste_menus.txt", "r"); //Ouvre notre.txt de stockage de menus
     int i=0;
 
     if(fichier != NULL)
     {
     	while(fgets(ligne,30,fichier)!= NULL &&i<nbrMenu)
     	{
-        	fscanf(fichier,"/----\n%s\n%s\n%s\n%s\n%s\n%s\n----/", listeMenu[i].menu1, listeMenu[i].menu2, listeMenu[i].menu3, listeMenu[i].menu4, listeMenu[i].menu5, listeMenu[i].menu6);
+        	fscanf(fichier,"/----\n%s\n%s\n%s\n%s\n%s\n%s\n----/", listeMenu[i].menu1, listeMenu[i].menu2, listeMenu[i].menu3, listeMenu[i].menu4, listeMenu[i].menu5, listeMenu[i].menu6); //Stocke les menus dans un tableau de structure correspondant à ceux-ci
 
         	conversionUE(listeMenu[i].menu1);
         	conversionUE(listeMenu[i].menu2);
